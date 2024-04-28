f8027236a8828697e2eec7b79a5474c2d7342948
AniketShark <aniket.kayande@gmail.com>
2024-04-28 09:32:17 +0530

    [Fixed] Netcode.Common was Ingnored in git

---
+++ Netcode.Common/EventType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netcode.Common
+{
+    public enum EventType
+    {
+        None = 0,
+        RegisterPlayer = 1,
+        Input = 3,
+        Custom = 4,
+        UpdateLobby = 5
+    }
+}
